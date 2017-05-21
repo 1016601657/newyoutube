4e5c9eab5aaec66aacab0a8803d63db7dc7e06e6
LJS <13120321516@163.com>
2017-05-22 01:02:01 +0800

    video

--- fetchChannelVideos.php
+++ fetchChannelVideos.php
@@ -12,6 +12,7 @@ $youtube = new youtube();
 // 从数据库获取is_get_detail为0的数据
 $ytbRes = $youtube->db->select('ytb_channels', ['id','user_url'], ['is_get_video'=>0]);
 foreach($ytbRes as $k => $v){
+    $v['user_url'] = str_replace('about','videos',$v['user_url']);
     $youtube->init($v['user_url']);
     $videoLinks = $youtube->get_video_link();
     $videoDetail = [];

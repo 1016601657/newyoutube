2ad3e3f0ba925c0cb0df6853d1f875db9b49dcec
LJS <13120321516@163.com>
2017-05-31 15:48:10 +0800

    hhd

--- fetchChannelInfo.php
+++ fetchChannelInfo.php
@@ -8,44 +8,44 @@
 // 引入youtube类
 include 'youtube.class.php';
 $youtube = new youtube();
-while(1){
+while (1) {
 // 从数据库获取is_get_detail为0的数据
 //$ytbRes = $youtube->db->select('ytb_channels', 'ytb_id', ['is_get_detail'=>0]);
-$ytbRes = $youtube->db->select('ytb_channels', 'ytb_id', ['AND'=>['is_get_video'=>0,'keywords'=>['unboxing','reviews','gadgets','smart tech']]]);
-if(count($ytbRes) == 0){
-exit;
-}
-foreach($ytbRes as $k => $v){
-    if(strlen($v) == 24){
-        $url = 'https://www.youtube.com/channel/'.$v.'/about';
-        $ytb_type = 'channel';
-    }else{
-        $url = 'https://www.youtube.com/user/'.$v.'/about';
-        $ytb_type = 'user';
+    $ytbRes = $youtube->db->select('ytb_channels', 'ytb_id', ['AND' => ['is_get_video' => 0, 'keywords' => ['unboxing', 'reviews', 'gadgets', 'smart tech']]]);
+    if (count($ytbRes) == 0) {
+        exit;
     }
-    $youtube->init($url);
-    $youtubeInfo['ytb_type'] = $ytb_type;
-    $youtubeInfo['username'] = $youtube->get_channel_name();
-    $youtubeInfo['subscribers'] = $youtube->get_subscribers();
-    $youtubeInfo['views'] = $youtube->get_views();
-    $youtubeInfo['country'] = $youtube->get_country();
-    $youtubeInfo['user_created'] = $youtube->get_join_time();
-    $youtubeInfo['user_url'] = $youtube->url;
-    $friendLink = $youtube->get_links();
-    $youtubeInfo['user_twitter'] = $friendLink['twitter'];
-    $youtubeInfo['user_instagram'] = $friendLink['instagram'];
-    $youtubeInfo['user_facebook'] = $friendLink['facebook'];
-    $youtubeInfo['created'] = time();
-    $youtubeInfo['is_get_detail'] = 1;
-    $id = $youtube->db->update("ytb_channels", $youtubeInfo, ['ytb_id'=>$v]);
+    foreach ($ytbRes as $k => $v) {
+        if (strlen($v) == 24) {
+            $url = 'https://www.youtube.com/channel/' . $v . '/about';
+            $ytb_type = 'channel';
+        } else {
+            $url = 'https://www.youtube.com/user/' . $v . '/about';
+            $ytb_type = 'user';
+        }
+        $youtube->init($url);
+        $youtubeInfo['ytb_type'] = $ytb_type;
+        $youtubeInfo['username'] = $youtube->get_channel_name();
+        $youtubeInfo['subscribers'] = $youtube->get_subscribers();
+        $youtubeInfo['views'] = $youtube->get_views();
+        $youtubeInfo['country'] = $youtube->get_country();
+        $youtubeInfo['user_created'] = $youtube->get_join_time();
+        $youtubeInfo['user_url'] = $youtube->url;
+        $friendLink = $youtube->get_links();
+        $youtubeInfo['user_twitter'] = $friendLink['twitter'];
+        $youtubeInfo['user_instagram'] = $friendLink['instagram'];
+        $youtubeInfo['user_facebook'] = $friendLink['facebook'];
+        $youtubeInfo['created'] = time();
+        $youtubeInfo['is_get_detail'] = 1;
+        $id = $youtube->db->update("ytb_channels", $youtubeInfo, ['ytb_id' => $v]);
 
-    $recommendLinks = $youtube->get_recommend_links();
-    // 检查是否获取过
-    foreach($recommendLinks as $r_k){
-        $isExist = $youtube->db->select('ytb_channels','id',['ytb_id'=>$r_k]);
-        if(!$isExist){
-            $ytbRes = $youtube->db->insert('ytb_channels',['ytb_id'=>$r_k]);
+        $recommendLinks = $youtube->get_recommend_links();
+        // 检查是否获取过
+        foreach ($recommendLinks as $r_k) {
+            $isExist = $youtube->db->select('ytb_channels', 'id', ['ytb_id' => $r_k]);
+            if (!$isExist) {
+                $ytbRes = $youtube->db->insert('ytb_channels', ['ytb_id' => $r_k]);
+            }
         }
     }
-}
 }
--- fetchChannelVideos.php
+++ fetchChannelVideos.php
@@ -8,36 +8,36 @@
 // 引入youtube类
 include 'youtube.class.php';
 $youtube = new youtube();
-while(1){
+while (1) {
 // 从数据库获取is_get_detail为0的数据
 //$ytbRes = $youtube->db->select('ytb_channels', ['id','user_url'], ['is_get_video'=>0]);
-$ytbRes = $youtube->db->select('ytb_channels', ['id','user_url'], ['AND'=>['is_get_video'=>0,'keywords'=>['unboxing','reviews','gadgets','smart tech']]]);
-if(count($ytbRes) == 0){
-exit;
-}
-foreach($ytbRes as $k => $v){
-    $v['user_url'] = str_replace('about','videos',$v['user_url']);
-    $youtube->init($v['user_url']);
-
-    $videoLinks = $youtube->get_video_link();
-    $videoDetail = [];
-    $videotype = [];
-    foreach($videoLinks as $u_k){
-        $detail = [];
-        $detailUrl = 'https://www.youtube.com'.$u_k;
-        $youtube->init($detailUrl);
-        $detail['channel_id'] = $v['id'];
-        $detail['title'] = $youtube->get_video_title();
-        $detail['upload_time'] = $youtube->get_video_upload();
-        $detail['view'] = $youtube->get_video_view();
-        $detail['type'] = $youtube->get_video_type();
-        $videotype[$detail['type']]++;
-        $res = $youtube->db->insert("ytb_video", $detail);
+    $ytbRes = $youtube->db->select('ytb_channels', ['id', 'user_url'], ['AND' => ['is_get_video' => 0, 'keywords' => ['unboxing', 'reviews', 'gadgets', 'smart tech']]]);
+    if (count($ytbRes) == 0) {
+        exit;
     }
-    arsort($videotype);
-    reset($videotype);
-    $first_key = key($videotype);
-    $youtube->db->update("ytb_channels", ['type'=>$first_key,'is_get_video'=>1],['id'=>$v['id']]);
-}
+    foreach ($ytbRes as $k => $v) {
+        $v['user_url'] = str_replace('about', 'videos', $v['user_url']);
+        $youtube->init($v['user_url']);
+
+        $videoLinks = $youtube->get_video_link();
+        $videoDetail = [];
+        $videotype = [];
+        foreach ($videoLinks as $u_k) {
+            $detail = [];
+            $detailUrl = 'https://www.youtube.com' . $u_k;
+            $youtube->init($detailUrl);
+            $detail['channel_id'] = $v['id'];
+            $detail['title'] = $youtube->get_video_title();
+            $detail['upload_time'] = $youtube->get_video_upload();
+            $detail['view'] = $youtube->get_video_view();
+            $detail['type'] = $youtube->get_video_type();
+            $videotype[$detail['type']]++;
+            $res = $youtube->db->insert("ytb_video", $detail);
+        }
+        arsort($videotype);
+        reset($videotype);
+        $first_key = key($videotype);
+        $youtube->db->update("ytb_channels", ['type' => $first_key, 'is_get_video' => 1], ['id' => $v['id']]);
+    }
 }
 
